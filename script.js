6fc54e2a281df4387e10116386a57d5227f3fba7
Jhansi R <97968506+9980797290@users.noreply.github.com>
2022-03-04 16:38:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// Decision making
+// for one condition
+// let a=10;
+// if(a==10){
+    // success}
+    // else{
+        // failure
+        // }
+
+        // if else ladder 
+        // two condition
+        // let a=10;
+//         if(a<0{
+//             console.log("negative");
+//  }else if(a==0){
+//      console.log("zero")
+//  }else{
+//      console.log("positive")
+//  }
+//  Three or more conditions
+// var age=18;
+
+
+// switch (age){
+//     case  age<18:
+//          console.log("Child");
+//          break;
+//     case age>18:
+//          console.log("Adult");
+//         break;
+//         default:
+//            console.log("Old age");   
+//             break;
+// }
+// let age = 10;
+
+// switch (true) {
+//         case age < 18:
+//                 console.log("Child");
+//                 break;
+//         case 18 < age < 40:
+//                 console.log("Adult");
+//                 break;
+//         default:
+//                 console.log("old age");
+//                 break;
+// }
+
+// let day=1;
+// switch(day){
+//     case day =0:
+//     console.log("Monday");
+//     break;
+//     case day =1:
+//     console.log("Tuesday");
+//     break;
+//     case day =2:
+//     console.log("Wednesday");
+//     break;
+//      case day =3:
+//     console.log("Thursday");
+//     break;
+//      case day =4:
+//     console.log("Friday");
+//     break;
+//     case day=5:
+//      console.log("Saturday")
+//     break;
+//      default:
+//      console.log("Sunday")
+// }
+// Loops-for,while,do while
+// Iteration statements
+// 3 values
+// initialization
+// condition
+// increment /decrement
+//             var a;
+//             var a=12;
+//             We are initializing the variable with the value of 12;
+//             for(let i=1; i<=0;i++){
+//                 console.log(i);
+//             }
+
+
+
+
+
